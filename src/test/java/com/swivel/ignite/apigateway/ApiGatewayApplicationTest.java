7977e63a8747c92c00bfb4186df3ae56786347ac
Nawaz-swivel <nawas@swivelgroup.com.au>
2023-04-02 00:30:18 +0530

    changed urls

--- src/test/java/com/swivel/ignite/apigateway/ApiGatewayApplicationTest.java
+++ src/test/java/com/swivel/ignite/apigateway/ApiGatewayApplicationTest.java
@@ -14,10 +14,10 @@ class ApiGatewayApplicationTest {
     /**
      * This method tests Spring application main run method
 //     */
-//    @Test
-//    void Should_RunSpringApplication() {
-//        ApiGatewayApplication.main(new String[]{});
-//
-//        assertTrue(true, "Spring Application Context Loaded Successfully");
-//    }
+    @Test
+    void Should_RunSpringApplication() {
+        ApiGatewayApplication.main(new String[]{});
+
+        assertTrue(true, "Spring Application Context Loaded Successfully");
+    }
 }
